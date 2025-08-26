40e7052a90228e83047be249bb64d3e67e6592ee
abhay-mishra18 <abhaymishrajp@gmail.com>
2025-08-26 17:55:51 +0530

    practice-codes

---
+++ code-practice.py
@@ -0,0 +1,11 @@
+# to find the factorial of any number 
+
+n= int(input("Enter a number : "))
+
+product=1
+for i in range (1,n+1):
+    product=product*i 
+    i=i+1
+
+print(f"The factorial of {n} is {product}")
+
