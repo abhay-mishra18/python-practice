e4df04bb0e607272a3b3ece4f25f763d013d7d26
abhay-mishra18 <abhaymishrajp@gmail.com>
2025-09-07 23:28:43 +0530

    lesson update

--- code-practice.py
+++ code-practice.py
@@ -9,3 +9,22 @@ for i in range (1,n+1):
 
 print(f"The factorial of {n} is {product}")
 
+# code to print the table of n number in reversed order 
+
+n=int(input("Enter a number :" ))
+for i in range(1,11):
+    print(f"{n} X {11-i} = {n*(11-i)}")
+ 
+#factorial of any number by method of recursion 
+
+def factorial(n):
+    if (n==1 or n==0):
+        return 1
+    return(n * factorial(n-1))
+
+n=int(input("Enter a number :"))
+print(f"The factorial of {n} is : {factorial(n)}")
+
+
+
+
